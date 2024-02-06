372e0e69885269d5dbe7bde6898e864a1a4c8c9c
99hansling <lxhan@stu.pku.edu.cn>
2024-02-07 02:47:02 +0800

    initial code

--- snake.js
+++ snake.js
@@ -1,244 +1,80 @@
-// 设置画布
-var canvas = document.getElementById('game');
-var context = canvas.getContext('2d');
-var obstacles = [];
-// 设置网格大小和游戏大小
-var grid = 16;
-var count = 0;
-var score = 0;
-// 创建一个新的 Audio 对象
-var eatSound = new Audio('./eat.mp3');
-// 设置蛇
-var snake = {
-  x: 160,
-  y: 160,
-  
-  // 蛇移动的速度
-  dx: grid,
-  dy: 0,
-  
-  // 保存蛇身体的位置
-  cells: [],
-  
-  // 蛇的长度
-  maxCells: 4
-};
+let canvas = document.getElementById('game');
+let context = canvas.getContext('2d');
 
-// 设置苹果
-var apple = {
-  x: 320,
-  y: 320
-};
-function gameOver() {
+let box = 32;
+let snake = [];
+snake[0] = { x: 8 * box, y: 8 * box };
 
-    // 重置蛇的位置和方向
-    snake.x = canvas.width / 2;
-    snake.y = canvas.height / 2;
-    snake.dx = grid;
-    snake.dy = 0;
-    
-    // 重置蛇的长度
-    snake.cells = [];
-    snake.maxCells = 4;
-    
-    // 重新生成苹果和障碍物
-    apple.x = getRandomInt(0, 25) * grid;
-    apple.y = getRandomInt(0, 25) * grid;
-    obstacles = [];
-    for (var i = 0; i < 10; i++) {
-        createObstacle();
-    }
-    
-    // 显示游戏结束的弹窗，并刷新页面
-    alert('游戏结束');
-    location.reload();
-      
-  }
-// 随机生成苹果的位置
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-function createObstacle() {
-    var obstacle = {
-        x: getRandomInt(0, 25) * grid,
-        y: getRandomInt(0, 25) * grid
-    };
-
-    obstacles.push(obstacle);
-}
-for (var i = 0; i < 10; i++) {
-    createObstacle();
-}
-// 游戏循环
-function loop() {
-  requestAnimationFrame(loop);
-  
-  // 控制游戏速度
-  if (++count < 4) {
-    return;
-  }
-
-
-  count = 0;
-  context.clearRect(0,0,canvas.width,canvas.height);
-
-  // 移动蛇
-  snake.x += snake.dx;
-  snake.y += snake.dy;
-
-  // 如果蛇超出边界，从另一边出现
-  if (snake.x < 0) {
-    snake.x = canvas.width - grid;
-  }
-  else if (snake.x >= canvas.width) {
-    snake.x = 0;
-  }
-  
-  if (snake.y < 0) {
-    snake.y = canvas.height - grid;
-  }
-  else if (snake.y >= canvas.height) {
-    snake.y = 0;
-  }
-
-  // 保存蛇的位置
-  snake.cells.unshift({x: snake.x, y: snake.y});
-
-  // 如果蛇的长度超过最大长度，删除最后一个元素
-  if (snake.cells.length > snake.maxCells) {
-    snake.cells.pop();
-  }
-  obstacles.forEach(function(obstacle) {
-    context.fillStyle = 'black';
-    context.fillRect(obstacle.x, obstacle.y, grid-1, grid-1);
-});
-  // 绘制苹果
-  context.fillStyle = 'red';
-  context.fillRect(apple.x, apple.y, grid-1, grid-1);
-
-  // 绘制蛇
-  context.fillStyle = 'green';
-  snake.cells.forEach(function(cell, index) {
-
-
-    // 绘制蛇身体
-  context.fillRect(cell.x, cell.y, grid-1, grid-1);  
-
- 
-    // 如果蛇吃到苹果
-    if (cell.x === apple.x && cell.y === apple.y) {
-      score++;  // 添加这行代码
-      snake.maxCells++;
-      eatSound.play();  // 播放音频
-      
-      // 随机生成新的苹果位置
-      apple.x = getRandomInt(0, 25) * grid;
-      apple.y = getRandomInt(0, 25) * grid;
-    }
-    
-    obstacles.forEach(function(obstacle) {
-        if (snake.x === obstacle.x && snake.y === obstacle.y) {
-            // 蛇碰到了障碍物，游戏结束
-            gameOver();
-        }
-    });
-
-    // 检查蛇是否碰到自己
-    for (var i = index + 1; i < snake.cells.length; i++) {
-        
-        // 如果碰到自己，重新开始游戏
-        if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-            gameOver();
-
-            snake.x = 160;
-            snake.y = 160;
-            snake.cells = [];
-            snake.maxCells = 4;
-            snake.dx = grid;
-            snake.dy = 0;
-            
-            // 随机生成新的苹果位置
-            apple.x = getRandomInt(0, 25) * grid;
-            apple.y = getRandomInt(0, 25) * grid;
-        }
-    }
-  });
-    // 绘制分数
-    context.fillStyle = 'white';
-    context.font = '16px Arial';
-    context.fillText('Score: ' + score, 8, 20);
+let direction = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
 }
 
+function createBG() {
+  context.fillStyle = "lightgreen";
+  context.fillRect(0, 0, 16 * box, 16 * box);
+}
 
-var touchStartX = 0;
-var touchStartY = 0;
+function createSnake() {
+  for (i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
 
-// 监听 touchstart 事件
-canvas.addEventListener('touchstart', function(e) {
-    touchStartX = e.touches[0].clientX;
-    touchStartY = e.touches[0].clientY;
-}, false);
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
+}
 
-// 监听 touchmove 事件
-canvas.addEventListener('touchmove', function(e) {
-    var touchEndX = e.changedTouches[0].clientX;
-    var touchEndY = e.changedTouches[0].clientY;
-    var diffX = touchStartX - touchEndX;
-    var diffY = touchStartY - touchEndY;
+document.addEventListener('keydown', update);
 
-    if (Math.abs(diffX) > Math.abs(diffY)) {
-        // 左右滑动
-        if (diffX > 0) {
-            // 左滑动
-            snake.dx = -grid;
-            snake.dy = 0;
-        } else {
-            // 右滑动
-            snake.dx = grid;
-            snake.dy = 0;
-        }
-    } else {
-        // 上下滑动
-        if (diffY > 0) {
-            // 上滑动
-            snake.dy = -grid;
-            snake.dx = 0;
-        } else {
-            // 下滑动
-            snake.dy = grid;
-            snake.dx = 0;
-        }
+function update(event) {
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function startGame() {
+  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+  for (i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+      clearInterval(game);
+      alert('Game Over :(');
     }
-
-    e.preventDefault();
-}, { passive: false });  // 注意这里的 { passive: false }
-
-
-
-// 监听键盘事件，控制蛇的移动方向
-document.addEventListener('keydown', function(e) {
-  
-  // 左箭头
-  if (e.which === 37 && snake.dx === 0) {
-    snake.dx = -grid;
-    snake.dy = 0;
   }
-  // 上箭头
-  else if (e.which === 38 && snake.dy === 0) {
-    snake.dy = -grid;
-    snake.dx = 0;
-  }
-  // 右箭头
-  else if (e.which === 39 && snake.dx === 0) {
-    snake.dx = grid;
-    snake.dy = 0;
-  }
-  // 下箭头
-  else if (e.which === 40 && snake.dy === 0) {
-    snake.dy = grid;
-    snake.dx = 0;
-  }
-});
 
-// 开始游戏循环
-requestAnimationFrame(loop);
+  createBG();
+  createSnake();
+  drawFood();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
+
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
