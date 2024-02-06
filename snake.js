6832332590b44bda8946d7182e86389548aa1992
99hansling <lxhan@stu.pku.edu.cn>
2024-02-07 03:13:57 +0800

    new features

--- snake.js
+++ snake.js
@@ -1,9 +1,20 @@
 let canvas = document.getElementById('game');
 let context = canvas.getContext('2d');
-
+// 创建一个新的 Audio 对象并加载音频文件
+var eatSound = new Audio('eat.mp3');
 let box = 32;
 let snake = [];
 snake[0] = { x: 8 * box, y: 8 * box };
+
+// 创建障碍物数组
+var obstacles = [
+  { x: 5 * box, y: 7 * box },
+  { x: 8 * box, y: 10 * box },
+  { x: 9 * box, y: 5 * box },
+  // 添加更多障碍物...
+];
+
+
 
 let direction = "right";
 let food = {
@@ -28,6 +39,46 @@ function drawFood() {
   context.fillRect(food.x, food.y, box, box);
 }
 
+// 在游戏画布上绘制障碍物
+function drawObstacles() {
+  context.fillStyle = "blue";
+  for (var i = 0; i < obstacles.length; i++) {
+    context.fillRect(obstacles[i].x, obstacles[i].y, box, box);
+  }
+}
+// 检查蛇是否与任何障碍物碰撞
+function checkCollisionWithObstacles() {
+  for (var i = 0; i < obstacles.length; i++) {
+    if (snake[0].x == obstacles[i].x && snake[0].y == obstacles[i].y) {
+      // 蛇与障碍物碰撞，结束游戏
+      clearInterval(game);
+      alert('Game Over :(');
+    }
+  }
+}
+
+document.addEventListener('touchstart', function(e) {
+  var touch = e.touches[0];
+  this.startX = touch.clientX;
+  this.startY = touch.clientY;
+}, false);
+
+document.addEventListener('touchmove', function(e) {
+  var touch = e.touches[0];
+  var offsetX = touch.clientX - this.startX;
+  var offsetY = touch.clientY - this.startY;
+
+  if (Math.abs(offsetX) > Math.abs(offsetY)) {
+      // 水平移动
+      direction = offsetX > 0 ? 'right' : 'left';
+  } else {
+      // 垂直移动
+      direction = offsetY > 0 ? 'down' : 'up';
+  }
+}, false);
+
+// 在游戏逻辑中使用 `direction` 变量来控制蛇的移动方向
+
 document.addEventListener('keydown', update);
 
 function update(event) {
@@ -48,11 +99,14 @@ function startGame() {
       clearInterval(game);
       alert('Game Over :(');
     }
+
   }
 
   createBG();
   createSnake();
   drawFood();
+  drawObstacles();
+  checkCollisionWithObstacles();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -67,6 +121,7 @@ function startGame() {
   } else {
     food.x = Math.floor(Math.random() * 15 + 1) * box;
     food.y = Math.floor(Math.random() * 15 + 1) * box;
+    eatSound.play();
   }
 
   let newHead = {
